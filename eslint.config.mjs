90303ac7d11374e88bedfdcad02283d16b52e3cd
Vedant Sonkar <vedantsonkar0810@gmail.com>
2025-03-24 16:45:51 +0530

    Update Packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,9 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignorePatterns: ["node_modules/"],
-  },
+  { ignores: ["node_modules/*"] },
 ];
 
 export default eslintConfig;
