51badc6d32b8409a95de0878be36552100b68524
Vedant Sonkar <vedantsonkar0810@gmail.com>
2025-03-24 13:33:22 +0530

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 import Header from "@/components/Navbar/Header";
@@ -34,6 +35,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
