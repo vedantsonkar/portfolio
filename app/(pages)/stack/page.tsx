1477b143fdc336cee6b7134d9cace4ae28488c42
Vedant Sonkar <vedantsonkar0810@gmail.com>
2025-10-19 03:21:46 +0530

    Fix tech stack

--- app/(pages)/stack/page.tsx
+++ app/(pages)/stack/page.tsx
@@ -1,7 +1,10 @@
 export default function StackPage() {
   return (
-    <main className="font-[family-name:var(--font-geist-sans)]">
-      <div className="mx-auto max-w-7xl [1600px]:max-w-9xl px-4 sm:px-6 lg:px-8 py-24 text-center top-1/2 transform translate-y-1/2">
+    <main
+      className="font-[family-name:var(--font-geist-sans)] min-h-screen"
+      style={{ background: "var(--background)" }}
+    >
+      <div className="mx-auto max-w-7xl [1600px]:max-w-9xl px-4 sm:px-6 lg:px-8 py-24 text-center">
         <h1 className="text-6xl sm:text-8xl [1600px]:text-9xl font-bold">
           Tech Stack Coming Soon!
         </h1>
